d4adb5e36fa3cc63c8021bb2329aecc9d3750aaf
agorenst <agorenst@outlook.com>
2017-01-02 18:05:46 -0800

    NCB (Non-chronological backtracking) implemented
    As detailed in section 4.3 of the D&N paper. On my machine, this reduces
    the aim-*50* benchmark from 2.4-ish seconds to 1.4-ish seconds. That's a
    huge improvement.
    
    There are some small complications: if the newparent clause is a unit
    clause it's not obvious if things are benificial. Also, my
    ChooseNextVariable function is incredibly simple, which would lead the
    NCB into infinite loops in some instances. I work-around this by
    preventing NCB in cases where we seem to be re-approaching our original
    decision sequence.
    
    Note that even with these hacky gaps, we get a real performance
    improvement! Very nice.

--- RunAim.sh
+++ RunAim.sh
@@ -1,6 +1,5 @@
 for f in inputs/aim*-50*
 #for f in inputs/aim*
 do
-    echo "About to do " $f
     ./sat < $f
 done
--- sat.cpp
+++ sat.cpp
@@ -51,11 +51,62 @@ bool solve(cnf_table& c) {
         literal next_decision = d.decisions[d.level];
         d.left_right[d.level] = L;
         a.set_true(next_decision);
-        
+
         for (;;) {
             // Find a conflict clause if it exists
-            auto parent_clause = find_conflict_augment(c, new_parent, a);
+            const auto parent_clause = find_conflict_augment(c, new_parent, a);
             if (!parent_clause.size()) { break; }
+
+            //////////////////////////////////////////////////////////////////
+            // NCB STARTS HERE
+            // Extra conditional: make sure the conflict is worthwhile...
+            if (parent_clause.size() > 1) {
+                int g = d.level;
+                auto NCB_clause = parent_clause;
+                ASSERT(NCB_clause.contains(-d.decisions[g]));
+                NCB_clause.erase(-d.decisions[g]);
+
+                // find the minimal g such that the NCB clause is still unsat...
+                while (is_clause_unsatisfied(NCB_clause, a)) {
+                    a.unassign(d.decisions[g]);
+                    --g;
+                }
+                ++g;
+                ASSERT(!is_clause_unsatisfied(NCB_clause, a));
+                a.set_true(d.decisions[g]);
+                ASSERT(is_clause_unsatisfied(NCB_clause, a));
+
+
+                // OPTIONAL, continue backtracking up implied literals.
+                //for (--g; d.left_right[g] == R; --g);
+
+                // Now we finish backtracking:
+                ASSERT(g+1 <= d.level);
+                // To avoid infinite looping, we only backtrack when we're reordering
+                // decision variables "backwards". This is built off the idea that we
+                // assign literals from abs(1) ... abs(max_variable), and avoids
+                // us getting in an infinite loop where we swap -8 with 14 and then
+                // 14 with -8. This would come up in the UNSAT cases.
+                if (std::abs(d.decisions[g+1]) >= std::abs(d.decisions[d.level])) {
+                    // NCB is bad,
+                    // Reset all our NCB.
+                    for (; g+1 < d.level; ++g) {
+                        a.set_true(d.decisions[g+1]);
+                    }
+                    a.set_true(d.decisions[g+1]);
+                    ASSERT(g+1 == d.level);
+                }
+                else {
+                    // NCB is good,
+                    // Finish our NCB:
+                    std::swap(d.decisions[g+1],d.decisions[d.level]);
+                    a.set_true(d.decisions[g+1]);
+                    d.level = g+1;
+                }
+            }
+            // NCB ENDS HERE
+            //////////////////////////////////////////////////////////////////
+
             Parent[d.level] = parent_clause;
             // The above lines basically transform the for(;;) loop into a
             // "while there is a conflict clause..." loop.
